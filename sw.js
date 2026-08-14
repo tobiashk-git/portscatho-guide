00934e1d68a9c8c997010381a56e1fa8b42c00fa
tobiashk-git <tobiaskeuneke@gmail.com>
2026-08-14 16:26:35 +0100

    Revert nature collage to original 6 images (crab+bass too busy); bump SW cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v6';
+const CACHE = 'portscatho-v7';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

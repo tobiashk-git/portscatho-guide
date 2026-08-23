fe9e8c327875c3e8bdd9700af70bbeba4dc01b95
tobiashk-git <tobiaskeuneke@gmail.com>
2026-08-23 16:36:39 +0100

    Header: animated weather scene background (sun/moon/clouds/rain/snow/storm), day-night aware

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v14';
+const CACHE = 'portscatho-v15';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

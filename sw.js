21951dc3228165d1669323776ac764608fe98966
tobiashk-git <tobiaskeuneke@gmail.com>
2026-08-23 16:44:40 +0100

    Header weather: atmospheric iOS-style scene (soft glows, blurred clouds, angled rain, flash)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v15';
+const CACHE = 'portscatho-v16';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

4faa0a1ec02e15f2a41f121622005c4c699327bf
tobiashk-git <tobiaskeuneke@gmail.com>
2026-08-23 17:02:25 +0100

    Explore: add Plume of Feathers phone number (tap-to-call)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v18';
+const CACHE = 'portscatho-v19';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

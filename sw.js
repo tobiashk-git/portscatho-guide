f606be5ef27694d3767d8839bc205bbbf30554e9
tobiashk-git <tobiaskeuneke@gmail.com>
2026-08-25 19:45:35 +0100

    Family gallery: add 51 photos (2007-2026), rendered by year with count badges

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v19';
+const CACHE = 'portscatho-v20';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

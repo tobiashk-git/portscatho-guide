c9814d9e5414fa4eb097c70d60594b3b85976ac5
tobiashk-git <tobiaskeuneke@gmail.com>
2026-08-25 20:57:31 +0100

    Gallery collage: steeper downhill slant on decade labels

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v29';
+const CACHE = 'portscatho-v30';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

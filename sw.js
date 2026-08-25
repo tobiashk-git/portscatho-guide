58ad03e123cd51b31ea8be2b924117657626d602
tobiashk-git <tobiaskeuneke@gmail.com>
2026-08-25 20:20:10 +0100

    Gallery collage: clearer year layout (grid + paper halos, less overlap)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v24';
+const CACHE = 'portscatho-v25';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

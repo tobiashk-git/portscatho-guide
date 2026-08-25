d59196776a29fd0550e9c6967552991a6a03cde0
tobiashk-git <tobiaskeuneke@gmail.com>
2026-08-25 21:06:50 +0100

    Gallery collage: vertical decade text running parallel to the band slant

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v30';
+const CACHE = 'portscatho-v31';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

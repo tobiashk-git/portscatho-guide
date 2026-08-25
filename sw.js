e4f63b6ab17cd7251dbbfaec51be3f9176a5d6c7
tobiashk-git <tobiaskeuneke@gmail.com>
2026-08-25 19:51:18 +0100

    Disable pull-to-refresh on the hero banner to stop swipe jitter

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v20';
+const CACHE = 'portscatho-v21';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

826236c93cf93bd804ec44f486fa1a5fa37c7e0f
tobiashk-git <tobiaskeuneke@gmail.com>
2026-08-26 13:32:25 +0100

    Bottom nav: clearer active-tab highlight (tinted pill behind icon + bold label)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v53';
+const CACHE = 'portscatho-v54';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

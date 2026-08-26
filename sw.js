a8891ccf2afefbaf1e6994041721c5c14c774381
tobiashk-git <tobiaskeuneke@gmail.com>
2026-08-26 10:22:14 +0100

    Gallery: aged-to-vivid decade bars (sepia->sea-blue timeline)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v38';
+const CACHE = 'portscatho-v39';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

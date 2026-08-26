498a411873d3f8486f8b9c8648256cc6611ec7c7
tobiashk-git <tobiaskeuneke@gmail.com>
2026-08-26 13:27:55 +0100

    Gallery: replace 2025 p01 (IMG_4131) with full-resolution version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v52';
+const CACHE = 'portscatho-v53';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

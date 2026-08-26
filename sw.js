f69271f9baff2557d47b2bc486988e49854136df
tobiashk-git <tobiaskeuneke@gmail.com>
2026-08-26 12:58:14 +0100

    Lightbox: swipe (and arrows/keys) to browse gallery photos without closing

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v49';
+const CACHE = 'portscatho-v50';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

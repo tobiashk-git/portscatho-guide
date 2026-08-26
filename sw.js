9f0817eff1143cae15c69c5fae6b8a5a26ecdf99
tobiashk-git <tobiaskeuneke@gmail.com>
2026-08-26 13:07:58 +0100

    Lightbox swipe: scope browsing to the single year's photos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v51';
+const CACHE = 'portscatho-v52';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

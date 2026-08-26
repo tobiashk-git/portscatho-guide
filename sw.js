e833df99b3248dd1c4b2bece9151416f62ca096c
tobiashk-git <tobiaskeuneke@gmail.com>
2026-08-26 11:51:49 +0100

    Harbour Club hours confirmed (daily 12-11pm, Fri/Sat to 1am); engine handles past-midnight sessions

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v44';
+const CACHE = 'portscatho-v45';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

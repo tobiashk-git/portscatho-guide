d15e619778843941aa643b26dedf617f68eb4495
tobiashk-git <tobiaskeuneke@gmail.com>
2026-08-26 09:11:31 +0100

    Feedback: tide card to top, gallery decade accordion, family hero photo, night tips, fuller landscape lightbox

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v32';
+const CACHE = 'portscatho-v33';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

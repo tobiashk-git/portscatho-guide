d70bf388e0cd9de2d724a607b40bdb32392922e9
tobiashk-git <tobiaskeuneke@gmail.com>
2026-08-30 08:12:34 +0100

    Banner tips: add 6-7pm dinnertime message (Plume daily; Boathouse fish & chips Thu/Fri/Sat)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v61';
+const CACHE = 'portscatho-v62';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

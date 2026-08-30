26d284c2ced2a13fa1f428e9389ad1cb0aab9e11
tobiashk-git <tobiaskeuneke@gmail.com>
2026-08-30 08:05:43 +0100

    Banner tips: gate evening 'pint at the Plume' to Plume hours (stops ~10:45pm last orders), then 'winding down'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v60';
+const CACHE = 'portscatho-v61';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

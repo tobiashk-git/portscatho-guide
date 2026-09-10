e60dd71c07de6ddcb9b7e482786156985e197e80
tobiashk-git <tobiaskeuneke@gmail.com>
2026-09-10 20:51:25 +0100

    gallery: add 1969 (7) and 1973 (1) photos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v71';
+const CACHE = 'portscatho-v72';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

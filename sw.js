25462e08d60fef237ef51d522761b9fd495f9552
tobiashk-git <tobiaskeuneke@gmail.com>
2026-09-10 20:32:02 +0100

    Add Holiday property info section (Morvoren) above practical tips

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v67';
+const CACHE = 'portscatho-v68';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

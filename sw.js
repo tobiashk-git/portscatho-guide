3c0c916397cc52af0aecda62801d8ff3651c49dc
tobiashk-git <tobiaskeuneke@gmail.com>
2026-09-10 21:19:36 +0100

    Rock pool: big photo reveal popup on discovering a creature

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v74';
+const CACHE = 'portscatho-v75';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

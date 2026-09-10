e10ce0e26133134456a93cc533ad6d651373bacc
tobiashk-git <tobiaskeuneke@gmail.com>
2026-09-10 20:34:29 +0100

    Name second property 5 Harbour Court

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v69';
+const CACHE = 'portscatho-v70';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

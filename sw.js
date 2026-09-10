a185f778c37e2dec999168a8c8621389025abfb9
tobiashk-git <tobiaskeuneke@gmail.com>
2026-09-10 21:07:32 +0100

    Fix Route 50 bus times: real bustimes.org timetable (Sep-Oct 2026)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v72';
+const CACHE = 'portscatho-v73';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {

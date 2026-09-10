dff5de9419651b60bfc54a0be59feba041665e30
tobiashk-git <tobiaskeuneke@gmail.com>
2026-09-10 20:40:25 +0100

    Bus panel: show Truro departures + Portscatho arrivals; add 1960s gallery decade

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portscatho-v70';
+const CACHE = 'portscatho-v71';
 const ASSETS = ['index.html', 'manifest.webmanifest', 'icons/icon.svg', 'tides.json'];
 
 self.addEventListener('install', e => {
